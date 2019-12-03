e3ea764422ef7522b344d67e68487eb76c98829a
yinshiheng <49746837+yinshiheng@users.noreply.github.com>
2019-12-04 00:10:19 +0900

    Add files via upload

---
+++ Scripts/LookCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCamera : MonoBehaviour {
+
+
+
+	void Update(){
+
+		transform.LookAt (Camera.main.transform.position);
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Scripts/SetName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetName : MonoBehaviour {
+
+
+	public string thisName;
+	void Start(){
+		transform.name = thisName;
+
+	}
+
+
+
+
+
+
+
+
+
+}
